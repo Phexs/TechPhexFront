00f18227a3868572d39d9552a65f8aec458ca5ca
Osward Phekani <phexyosd@gmail.com>
2019-03-28 01:50:48 +0000

    added click events for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'TechPhex';
+    onClick($event){
+    
+    console.log(onclick);
+    const x = document.getElementById("hideBlock");
+    if(x.style.display === "none"){
+      x.style.display = "block";
+    }else{
+      x.style.display = "none";
+    }
+  }
+ 
 }
