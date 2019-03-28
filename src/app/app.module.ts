74d3f56a71762aea89272675cd8b3e95c47a4bbb
Osward Phekani <phexyosd@gmail.com>
2019-03-28 12:52:54 +0000

    added Client,consultant and employee Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { ConsultantComponent } from './consultant/consultant.component';
+import { EmployeeComponent } from './employee/employee.component';
 // import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    ClientComponent,
+    ConsultantComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
