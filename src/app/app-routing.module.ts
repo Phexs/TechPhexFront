f7acf108b98f05daa85205b8a87706259edbd07b
Osward Phekani <phexyosd@gmail.com>
2019-04-20 19:00:25 +0100

    added default and wild card routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RegisterComponent } from './register/register.component';
 import { ClientComponent } from './client/client.component';
 import { ConsultantComponent } from './consultant/consultant.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'client', component: ClientComponent},
   {path: 'consultant', component: ConsultantComponent},
   {path: 'employee', component: EmployeeComponent},
+  {path: 'app', component: AppComponent},
+  {path: '**', component: PageNotFoundComponent},
 
 ];
 
@@ -19,7 +26,10 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [RegisterComponent,
+export const routingComponents = [
+    LoginComponent,
+    RegisterComponent,
     ClientComponent,
     ConsultantComponent,
-    EmployeeComponent];
+    EmployeeComponent,
+    PageNotFoundComponent];
